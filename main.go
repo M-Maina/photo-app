a41ba54c4949f3ba98e4ca0f1071788481567b96
G-Maina <python21developer>
2024-03-02 12:52:21 -0800

    Added 404 Page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to<a href=\"mailto:support@appsavvytalk.com\">support@appsavvytalk.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Sorry! We could not find what you were looking for")
 	}
 
 }
