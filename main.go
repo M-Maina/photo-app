391619c49b6ffd3fb45ab9279cfea9a26ec7f1b4
G-Maina <python21developer>
2024-03-02 13:45:26 -0800

    Created Home and Contact Page

--- main.go
+++ main.go
@@ -7,22 +7,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to<a href=\"mailto:support@appsavvytalk.com\">support@appsavvytalk.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "Sorry! We could not find what you were looking for")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to<a href=\"mailto:support@appsavvytalk.com\">support@appsavvytalk.com</a>.")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
