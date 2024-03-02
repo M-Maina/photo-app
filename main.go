65b41ec7cbff7d00646b9975b1eeb0e3e99dcc46
G-Maina <python21developer>
2024-03-02 14:03:58 -0800

    Added Custom 404 Page

--- main.go
+++ main.go
@@ -17,8 +17,14 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to<a href=\"mailto:support@appsavvytalk.com\">support@appsavvytalk.com</a>.")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, We couldn't find the page!</h1>")
+}
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
